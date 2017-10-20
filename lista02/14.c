c026d2bbf93d86840c0a96cc4ddeb5d98ecbe2e4
fmg07 <32197261+fmg07@users.noreply.github.com>
2017-10-19 23:12:31 -0300

    l02

--- lista02/14.c
+++ lista02/14.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
 
+int Palindromo(char *s)
+{
+    int i,k=strlen(s)-1;
+    for(i=0;i<strlen(s);i++){
+        if(s[i]!=s[k]) return 0;
+        k--;
+    }
+    return 1;
+
+}
+
+int main()
+{
+    char s[20];
+
+    scanf("%s",s);
+
+    int resultado=Palindromo(s);
+    if(resultado) printf("sim\n");
+    else printf("nao\n");
+
+    return 0;
+}
